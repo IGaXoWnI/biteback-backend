2bdb1755faa39c575c06844ff57ce0b204664673
ismail <igaxowni@gmail.com>
2025-04-21 14:48:56 +0100

    create crud methods for merchant to add offres the methods : index to get all offre  , store to add new offre  , show to get specifique offre by id

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BoxController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post("/login", [AuthController::class, 'login']);
-Route::post("/logout", [AuthController::class, 'logout'])->middleware("auth:api");
+Route::post("/logout", [AuthController::class, 'logout']);
 Route::post("/refresh", [AuthController::class, 'refresh'])->middleware("auth:api");
+
+Route::get("/boxes", [BoxController::class, 'index']);
+Route::post("/boxes", [BoxController::class, 'store']);
+Route::get("/boxes/{box}", [BoxController::class, 'show']);
+
+
+
+Route::post("user/update-location", [UserController::class, 'updateLocation'])
+    ->middleware('auth:api');
+
+
