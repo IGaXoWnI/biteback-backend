7a7fd32ee8a6f49f0830e94239fef2d1047ab180
ismail <igaxowni@gmail.com>
2025-04-25 15:06:08 +0100

    update the method the get all boxes in the search radius the user select

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,51 +12,52 @@ use Illuminate\Support\Facades\Validator;
 class UserController extends Controller
 {
 
-    
+    public function updateLocation(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'latitude' => 'required|string',
+            'longitude' => 'required|string',
+            'zone' => 'sometimes|string',
+        ]);
 
-public function updateLocation(Request $request)
-{
-    $validator = Validator::make($request->all(), [
-        'latitude' => 'required|string',
-        'longitude' => 'required|string',
-        'zone' => 'sometimes|string',
-    ]);
-    
-    $address = $this->getAddressFromCoordinates(
-        $request->latitude, 
-        $request->longitude
-    );
-    
-    $id = Auth::user()->id;
-    $user = User::find($id);
-    $user->latitude = $request->latitude;
-    $user->longitude = $request->longitude;
-    $user->zone = $request->zone ?? $user->zone;
-    $user->address = $address;
-    $user->save();
-    
-    return response()->json([
-        'success' => true,
-        'message' => 'Location updated successfully',
-        'data' => [
-            'user' => $user,
-            'address' => $address
-        ]
-    ]);
-}
+        $address = $this->getAddressFromCoordinates(
+            $request->latitude,
+            $request->longitude
+        );
 
-private function getAddressFromCoordinates($latitude, $longitude)
-{
-    $apiKey = '93c44a46f2924bbc886b818dcf32aea3'; 
-    $url = "https://api.opencagedata.com/geocode/v1/json?q={$latitude}+{$longitude}&key={$apiKey}";
+        $id = Auth::user()->id;
+        $user = User::find($id);
+        $user->latitude = $request->latitude;
+        $user->longitude = $request->longitude;
+        $user->zone = $request->zone ?? $user->zone;
+        $user->address = $address;
+        $user->save();
 
-    $response = Http::get($url);
-    $data = $response->json();
-
-    if (isset($data['results'][0]['formatted'])) {
-        return $data['results'][0]['formatted'];
+        return response()->json([
+            'success' => true,
+            'message' => 'Location updated successfully',
+            'data' => [
+                'user' => $user,
+                'address' => $address
+            ]
+        ]);
     }
 
-    return null;
-}
+    private function getAddressFromCoordinates($latitude, $longitude)
+    {
+        $apiKey = '93c44a46f2924bbc886b818dcf32aea3';
+        $url = "https://api.opencagedata.com/geocode/v1/json?q={$latitude}+{$longitude}&key={$apiKey}";
+
+        $response = Http::get($url);
+        $data = $response->json();
+
+        if (isset($data['results'][0]['formatted'])) {
+            return $data['results'][0]['formatted'];
+        }
+
+        return null;
+    }
+
+ 
+   
 }
